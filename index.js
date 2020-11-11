8b13d3dfa961beee08977c13c0ab7f1d326d49e8
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-11-11 15:27:31 -0500

    Done.

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(src, fn) {
+      const srcCopy = Array.isArray(src) ? src.slice() : Object.values(src)
+      for (let i = 0; i < srcCopy.length; i++) {
+        fn(srcCopy[i])
+      }
+      return src;
     },
 
-    map: function() {
+    map: function(src, fn) {
+      const srcCopy = Array.isArray(src) ? src.slice() : Object.values(src);
+      let returnArray = [];
+
+      for (let i = 0; i < srcCopy.length; i++) {
+        returnArray.push(fn(srcCopy[i]))
+      }
+      return returnArray;
 
     },
 
